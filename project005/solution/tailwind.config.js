411e9fb64597667c6f45bd5ab5448060fe6c2334
Szymon Oleszek <szymqwerty@gmail.com>
2023-10-15 03:41:32 +0200

    initial commit

---
+++ project005/solution/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-darker-blue": "#5847eb",
+        "custom-light-orange": "#ff8c66",
+        "custom-soft-blue": "#56c2e6",
+        "custom-light-red": "#ff5c7c",
+        "custom-lime-green": "#4acf81",
+        "custom-violet-social": "#7536d3",
+        "custom-soft-orange": "#f1c65b",
+        "custom-very-dark": "#0f1424",
+        "custom-dark-blue": "#1c1f4a",
+        "custom-desaturated-blue": "#6f76c8",
+        "custom-pale-blue": "#bdc1ff",
+      },
+    },
+  },
+  plugins: [],
+};
